7d077178e39180f036e1e7aecff5e89a05af97d0
Claus Guttesen <kometen@gmail.com>
2014-03-25 17:31:28 +0100

    Set active race

--- app/scripts/controllers/active_race.js
+++ app/scripts/controllers/active_race.js
@@ -1,22 +1,23 @@
 'use strict';
 
-app.controller('ActiveRaceCtrl', function ($location, $scope, $window, Race, User, ActiveRace) {
+app.controller('ActiveRaceCtrl', function ($location, $scope, $rootScope, $window, Race, User, ActiveRace) {
 
-	$scope.updateRace = function (raceId, race) {
-		Race.update(raceId, race);
-	};
+	if ($location.path() === '/') {
+		$scope.races = Race.all;
+		if (!$rootScope.activeRace) {
+			$rootScope.activeRace = ActiveRace.getRace();
+		}
+	}
 
 	$scope.currentUser = function () {
 		User.getCurrent();
 	};
 
-	$scope.selectRace = function (raceId, race) {
-		$window.alert('Selected race: ' + race.name + ', raceId: '+ raceId);
-		ActiveRace.setRace(raceId, race);
+	$scope.setActiveRace = function (raceId, race) {
+//		$window.alert('Selected race: ' + race.name + ', raceId: '+ raceId);
+		ActiveRace.setActiveRace(raceId, race);
 		$location.path('/#');
 	};
-
-	$scope.races = Race.all;
 
 	$scope.tabs = [
 		{ title: 'Available Races', content: 'Set active race from the list below'},
--- app/scripts/services/active_race.js
+++ app/scripts/services/active_race.js
@@ -1,19 +1,28 @@
 'use strict';
 
-app.factory('ActiveRace', function ($firebase, FIREBASE_URL, User) {
+app.factory('ActiveRace', function ($firebase, FIREBASE_URL, User, $rootScope, $window) {
 	var ref = new Firebase(FIREBASE_URL + 'active_race');
 	var ar = $firebase(ref);
 	var ActiveRace = {
-		setRace: function (raceId, race) {
+		setActiveRace: function (raceId, race) {
 			if (User.signedIn()) {
 				var user = User.getCurrent();
 				
 				race.owner = user.username;
 				ar.$child('raceId').$set(raceId);
 				ar.$child('race').$set(race);
+				$rootScope.activeRace = race;
+				$rootScope.activeRaceId = raceId;
 			}
 		},
+		getRace: function () {
+			ref.child('race').on('value', function (snapshot) {
+				console.log('name: ' + snapshot.val().name);
+				return snapshot.val().name + 'Claus';
+			});
+		},
 		getRaceId: function () {
+			$window.alert('race: ' + ar.$child('race'));
 			return ar.$child('raceId');
 		}
 	};
